6a4238efcdd342a4ab6db1a83442f9a18350ce9f
yaki <kiwidancebad@gmail.com>
2022-10-18 10:34:03 +0300

    update util

--- decorators/util.py
+++ decorators/util.py
@@ -7,7 +7,6 @@ def trace(func):
         print(f'trace: call {func.__name__}() with {args}, {kwargs}')
         func_result = func(*args, **kwargs)
         print(f'trace: {func.__name__}() resulted with {func_result!r}')
-
         return func_result
 
     return wrapper
