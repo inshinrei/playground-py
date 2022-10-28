54403e429b3c6acb6fadaf373ae5d1081f28a6d7
yaki <kiwidancebad@gmail.com>
2022-10-28 12:33:56 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-def apply_discount(price, discount):
-    price_with_discount = int(price * (1.0 - discount))
-    assert 0 <= price_with_discount <= price
-    return price_with_discount
-
-
 if __name__ == '__main__':
-    iterator = ("second text" for _ in range(5))
-    for text in iterator:
-        print(text)
