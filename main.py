59cc17ee2cd522365c46a3c1b6da658466be4274
yaki <kiwidancebad@gmail.com>
2022-11-04 22:30:26 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from matplotlib import pyplot as plt
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
-gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
-
-movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
-num_oscars = [5, 11, 3, 8, 10]
 
 if __name__ == '__main__':
-    plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
-    plt.title('nominal GDP')
-    plt.ylabel('$')
-    plt.show()
+
