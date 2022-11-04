e98de19803b8d85a3f12695228a8cbb8fed51769
yaki <kiwidancebad@gmail.com>
2022-11-04 22:39:45 +0300

    problem

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from typing import List
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+def two_sum(nums: List[int], target: int) -> List[int]:
+    seen = {}
+
+    for i, value in enumerate(nums):
+        remaining = target - nums[i]
+
+        if remaining in seen:
+            return [i, seen[remaining]]
+
+        seen[value] = i
 
 
 if __name__ == '__main__':
