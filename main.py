6a3506b991fb93bec099f00398f96b402ac25814
yaki <kiwidancebad@gmail.com>
2022-11-04 23:46:45 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from math import log
-
 """
 
 """
 
 
 if __name__ == '__main__':
-    def is_power_of_four(n: int) -> bool:
-        if n <= 0:
-            return False
 
-        return (log(n) / log(4)).is_integer()
-
-    print(is_power_of_four(16))
