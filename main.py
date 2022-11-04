bc96b1c14bc4900952d4d6cbee568d0666e9922f
yaki <kiwidancebad@gmail.com>
2022-11-04 22:39:55 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from typing import List
 
-
-# Input: nums = [2,7,11,15], target = 9
-# Output: [0,1]
-def two_sum(nums: List[int], target: int) -> List[int]:
-    seen = {}
-
-    for i, value in enumerate(nums):
-        remaining = target - nums[i]
-
-        if remaining in seen:
-            return [i, seen[remaining]]
-
-        seen[value] = i
 
 
 if __name__ == '__main__':
