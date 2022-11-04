1b177c8e3614a5f520013ccbea4cf59e193095cc
yaki <kiwidancebad@gmail.com>
2022-11-04 23:33:12 +0300

    cleanup

--- main.py
+++ main.py
@@ -8,15 +8,4 @@ Output: true
 
 
 if __name__ == '__main__':
-    def isBalanced(self, root) -> bool:
-        if root is None:
-            return True
 
-        def dfs(node):
-            if node is None:
-                return 0
-            return max(dfs(root.left), dfs(root.right)) + 1
-
-        return abs(
-            dfs(root.left) - dfs(root.right)
-        ) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
