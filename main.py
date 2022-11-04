827780d42ba2a09558c60bcfacf849676461da49
yaki <kiwidancebad@gmail.com>
2022-11-04 23:33:00 +0300

    height balanced b tree

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+"""
+Given a binary tree, determine if it is
+height-balanced
 
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+"""
 
 
 if __name__ == '__main__':
+    def isBalanced(self, root) -> bool:
+        if root is None:
+            return True
 
+        def dfs(node):
+            if node is None:
+                return 0
+            return max(dfs(root.left), dfs(root.right)) + 1
+
+        return abs(
+            dfs(root.left) - dfs(root.right)
+        ) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
