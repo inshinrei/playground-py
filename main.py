7c450fa9f8b9f0deed42acfe3b0aad6ee05b1555
yaki <kiwidancebad@gmail.com>
2022-11-07 12:02:06 +0000

    bar plot

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from matplotlib import pyplot as plt
 
+
+movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
 
 if __name__ == '__main__':
-
+    plt.bar(range(len(movies)), num_oscars)
+    plt.title('Movies')
+    plt.ylabel('# of academy awards')
+    plt.xticks(range(len(movies)), movies)
+    plt.show()
