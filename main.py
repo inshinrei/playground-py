3f08900a99bbd6bfa77f1b02b1aa608232a429cd
yaki <kiwidancebad@gmail.com>
2022-11-12 09:10:49 +0300

    bias variance

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ num_oscars = [5, 11, 3, 8, 10]
 grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
 mentions = [500, 505]
 years = [2017, 2018]
+variance     = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+total_error  = [x + y for x, y in zip(variance, bias_squared)]
+xs = [i for i, _ in enumerate(variance)]
+
 
 if __name__ == '__main__':
-    plt.bar(years, mentions, 0.8)
-    plt.xticks(years)
-    plt.ylabel('# of times')
-    plt.ticklabel_format(useOffset=False)
+    plt.plot(xs, variance, 'g-', label='variance')
+    plt.plot(xs, bias_squared, 'r-.', label='bias^2')
+    plt.plot(xs, total_error, 'b:', label='total error')
 
-    plt.axis([2016.5, 2018.5, 0, 550])
-    plt.title('tt')
+    plt.legend(loc=9)
+    plt.xlabel('model complexity')
+    plt.xticks([])
+    plt.title('bias var')
 
     plt.show()
