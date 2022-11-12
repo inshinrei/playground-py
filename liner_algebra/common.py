bceb9c896d078404388b67a69943627c5bf0e7ed
yaki <kiwidancebad@gmail.com>
2022-11-12 09:38:05 +0300

    add more vector functions

--- liner_algebra/common.py
+++ liner_algebra/common.py
@@ -1,3 +1,5 @@
+import math
+
 from typing import List
 
 
@@ -18,7 +20,7 @@ def subtract(v: Vector, w: Vector) -> Vector:
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 
 
-def vector_sum(vectors: List[Vector]) -> Vector:
+def vectors_sum(vectors: List[Vector]) -> Vector:
     assert vectors, 'no vectors provided'
 
     num_elements = len(vectors[0])
@@ -29,4 +31,34 @@ def vector_sum(vectors: List[Vector]) -> Vector:
 
 assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
 assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
-assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
+assert vectors_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
+
+
+def scalar_multiply(c: float, v: Vector) -> Vector:
+    return [c * v_i for v_i in v]
+
+
+def vectors_mean(vectors: List[Vector]) -> Vector:
+    n = len(vectors)
+    return scalar_multiply(1/n, vectors_sum(vectors))
+
+
+def dot(v: Vector, w: Vector) -> float:
+    assert len(v) == len(w), 'vectors must be the same len'
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v: Vector) -> float:
+    return dot(v, v)
+
+
+def magnitude(v: Vector) -> float:
+    return math.sqrt(sum_of_squares(v))
+
+
+assert scalar_multiply(2, [1, 2, 3]) == [2, 4, 6]
+assert vectors_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
+assert dot([1, 2, 3], [4, 5, 6]) == 32
+assert sum_of_squares([1, 2, 3]) == 14
+assert magnitude([3, 4]) == 5
