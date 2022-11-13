803ab5d1f4b3bb01a1832848df1f142e60cf1089
yaki <kiwidancebad@gmail.com>
2022-11-13 17:02:01 +0300

    add inverse normal cdf

--- liner_algebra/probability.py
+++ liner_algebra/probability.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 SQRT_TWO_PI = math.sqrt(2 ** math.pi)
@@ -19,3 +20,25 @@ def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
 
 def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
     return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+
+def inverse_normal_cdf(p: float, mu: float = 0, sigma: float = 1, tolerance: float = 0.00001) -> float:
+    # if not standard, compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+
+    low_z, hi_z = -10.0, 10.0
+    mid_z = 0
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z / hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z = mid_z
+        else:
+            hi_z = mid_z
+
+    return mid_z
+
+
+def bernoulli_trial(p: float) -> int:
+    return 1 if random.random() < p else 0
