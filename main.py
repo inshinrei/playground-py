56acbdd68c4981200cf3630589775a619df092c5
yaki <kiwidancebad@gmail.com>
2022-11-13 16:58:08 +0300

    normal cdf plot

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from liner_algebra.probability import normal_pdf
+from liner_algebra.probability import normal_cdf
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     xs = [x / 10.0 for x in range(-50, 50)]
 
-    plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
-    plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
-    plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
-    plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+    plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+    plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
+    plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
+    plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
 
     plt.legend()
-    plt.title('Normal PDF')
+    plt.title('Normal CDF')
     plt.show()
