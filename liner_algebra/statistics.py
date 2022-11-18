f724e35ec07ce000714708a7aeedafccf20ff99a
yaki <kiwidancebad@gmail.com>
2022-11-18 16:24:18 +0300

    add median function

--- liner_algebra/statistics.py
+++ liner_algebra/statistics.py
@@ -1,15 +1,23 @@
-import matplotlib.pyplot as plt
-from collections import Counter
+from typing import List
 
-num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-friend_counts =  Counter(num_friends)
-xs = range(101)
-ys = [friend_counts[x] for x in xs]
+def mean(xs: List[float]) -> float:
+    return sum(xs) / len(xs)
 
-plt.bar(xs, ys)
-plt.axis([0, 101, 0, 25])
-plt.title('counts')
-plt.xlabel('# of fr')
-plt.ylabel('# of people')
-plt.show()
+
+def _median_odd(xs: List[float]) -> float:
+    return sorted(xs)[len(xs) // 2]
+
+
+def _median_even(xs: List[float]) -> float:
+    sorted_xs = sorted(xs)
+    high_midpoint = len(xs) // 2
+    return (sorted_xs[high_midpoint - 1] + sorted_xs[high_midpoint]) / 2
+
+
+def median(v: List[float]) -> float:
+    return _median_even(v) if len(v) % 2 == 0 else _median_odd(v)
+
+
+assert median([1, 10, 2, 9, 5]) == 5
+assert median([1, 9, 2, 10]) == (2 + 9) / 2
