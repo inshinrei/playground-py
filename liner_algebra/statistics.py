c6ef7d1b4fa3924ac8c5c700843828d99342369e
yaki <kiwidancebad@gmail.com>
2022-11-18 16:49:23 +0300

    add some basic statistics function implementation

--- liner_algebra/statistics.py
+++ liner_algebra/statistics.py
@@ -1,4 +1,9 @@
+from collections import Counter
 from typing import List
+
+from liner_algebra.vector import sum_of_squares
+
+import math
 
 
 def mean(xs: List[float]) -> float:
@@ -21,3 +26,39 @@ def median(v: List[float]) -> float:
 
 assert median([1, 10, 2, 9, 5]) == 5
 assert median([1, 9, 2, 10]) == (2 + 9) / 2
+
+
+def quantile(xs: List[float], p: float) -> float:
+    p_index = int(p * len(xs))
+    return sorted(xs)[p_index]
+
+
+def mode(x: List[float]) -> List[float]:
+    counts = Counter(x)
+    max_count = max(counts.values)
+    return [x_i for x_i, count in counts.items() if count == max_count]
+
+
+def data_range(xs: List[float]) -> float:
+    return max(xs) - min(xs)
+
+
+def de_mean(xs: List[float]) -> List[float]:
+    x_bar = mean(xs)
+    return [x - x_bar for x in xs]
+
+
+def variance(xs: List[float]) -> float:
+    assert len(xs) >= 2, 'variance requires at least two elems'
+
+    n = len(xs)
+    deviations = de_mean(xs)
+    return sum_of_squares(deviations) / (n - 1)
+
+
+def standard_deviation(xs: List[float]) -> float:
+    return math.sqrt(variance(xs))
+
+
+def interquartile_range(xs: List[float]) -> float:
+    return quantile(xs, 0.75 - quantile(xs, 0.25))
