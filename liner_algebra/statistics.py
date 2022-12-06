9d22c9ee8d5747c8e29b401d4f02cde6d9e50574
yaki <kiwidancebad@gmail.com>
2022-12-06 14:05:17 +0300

    add experiment

--- liner_algebra/statistics.py
+++ liner_algebra/statistics.py
@@ -145,3 +145,13 @@ def two_sided_p_value(x: float, mu: float = 0, sigma: float = 1) -> float:
 
 def run_experiment() -> List[bool]:
     return [random.random() < 0.5 for _ in range(1000)]
+
+
+def reject_fairness(experiment: List[bool]) -> bool:
+    num_heads = len([flip for flip in experiment if flip])
+    return num_heads < 469 or num_heads > 531
+
+
+random.seed(0)
+experiments = [run_experiment() for _ in range(1000)]
+num_rejections = len([experiment for experiment in experiments if reject_fairness(experiment)])
