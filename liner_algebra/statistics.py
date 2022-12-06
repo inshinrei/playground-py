76189141bf897af3b5469b4e87a8fb5a1f6f2580
yaki <kiwidancebad@gmail.com>
2022-12-06 17:43:08 +0300

    add a b test statistic func

--- liner_algebra/statistics.py
+++ liner_algebra/statistics.py
@@ -156,3 +156,30 @@ random.seed(0)
 experiments = [run_experiment() for _ in range(1000)]
 num_rejections = len([experiment for experiment in experiments if reject_fairness(experiment)])
 assert num_rejections == 46
+
+
+def estimated_parameters(N: int, n: int) -> Tuple[float, float]:
+    p = n / N
+    sigma = math.sqrt(p * (1 - p) / N)
+    return p, sigma
+
+
+def a_b_test_statistic(N_A: int, n_A: int, N_B: int, n_B: int) -> float:
+    p_A, sigma_A = estimated_parameters(N_A, n_A)
+    p_B, sigma_B = estimated_parameters(N_B, n_B)
+    return (p_B - p_A) / math.sqrt(sigma_A ** 2 + sigma_B ** 2)
+
+
+a_b_test_0 = a_b_test_statistic(1000, 200, 1000, 180)
+assert -1.15 < a_b_test_0 < -1.13
+assert 0.253 < two_sided_p_value(a_b_test_0) < 0.255
+
+
+def B(alpha: float, beta: float) -> float:
+    return math.gamma(alpha) * math.gamma(beta) / math.gamma(alpha + beta)
+
+
+def beta_pdf(x: float, alpha: float, beta: float) -> float:
+    if x <= 0 or x >= 1:
+        return 0
+    return x ** (alpha - 1) * (1 - x) ** (beta - 1) / B(alpha, beta)
