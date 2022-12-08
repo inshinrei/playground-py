c883b1fe13cddccf93fd6a5f41be9c66df785439
yaki <kiwidancebad@gmail.com>
2022-12-08 13:12:37 +0300

    add gradient estimation

--- liner_algebra/gradient_descent.py
+++ liner_algebra/gradient_descent.py
@@ -1,4 +1,4 @@
-from typing import Callable, List
+from typing import Callable
 
 from liner_algebra.vector import Vector, dot
 
@@ -11,7 +11,7 @@ def difference_quotient(f: Callable[[float], float], x: float, h: float) -> floa
     return (f(x + h) - f(x)) / h
 
 
-def partial_difference_quotient(f: Callable[[List[float]], float], V: List[float], i: float, h: float) -> float:
+def partial_difference_quotient(f: Callable[[Vector], float], V: Vector, i: float, h: float) -> float:
     w = [v_j + (h if j == i else 0) for j, v_j in enumerate(V)]
     return (f(w) - f(V)) / h
 
@@ -22,3 +22,7 @@ def square(x: float) -> float:
 
 def derivative(x: float) -> float:
     return 2 * x
+
+
+def estimate_gradient(f: Callable[[Vector], float], V: Vector, h: float = 0.0001):
+    return [partial_difference_quotient(f, V, i, h) for i in range(len(V))]
