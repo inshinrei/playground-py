5edbbce468a6157a4ee81d012d06a74bbdb68a29
yaki <kiwidancebad@gmail.com>
2022-12-19 18:27:03 +0300

    add rescale function and some test cases

--- liner_algebra/pca.py
+++ liner_algebra/pca.py
@@ -11,3 +11,24 @@ def scale(data: List[Vector]) -> Tuple[Vector, Vector]:
     stdevs = [standard_deviation([vector[i] for vector in data]) for i in range(dim)]
 
     return means, stdevs
+
+
+vectors = [[-3, -1, 1], [-1, 0, 1], [1, 1, 1]]
+means_0, stdevs_0 = scale(vectors)
+
+assert means_0 == [-1, 0, 1]
+assert stdevs_0 == [2, 1, 0]
+
+
+def rescale(data: List[Vector]) -> List[Vector]:
+    dim = len(data[0])
+    means, stdevs = scale(data)
+
+    rescaled = [vector[:] for vector in data]
+
+    for vector in rescaled
+        for i in range(dim):
+            if stdevs[i] > 0:
+                v[i] = (v[i] - means[i]) / stdevs[i]
+
+    return rescaled
