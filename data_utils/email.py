cc424240e5ab05b35ca15736f9edc32beeb462ff
yaki <kiwidancebad@gmail.com>
2023-02-17 21:34:06 +0300

    cleanup

--- data_utils/email.py
+++
@@ -1,6 +0,0 @@
-def get_domain(email_address: str) -> str:
-    return email_address.lower().split('@')[-1]
-
-
-assert get_domain('kiwi@gmail.com') == 'gmail.com'
-assert get_domain('kiwi@icloud.com') == 'icloud.com'
--- learn/decorators.py
+++
@@ -1,5 +0,0 @@
-from typing import Optional
-
-
-def tag(name: str, /, *content: str, class_: Optional[str] = None, **attrs: str) -> str:
-    return repr((name, content, class_, attrs))
