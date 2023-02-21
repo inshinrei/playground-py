3c2fb0c3c2eef999ce1937461debdae22624b460
yaki <kiwidancebad@gmail.com>
2023-02-21 21:08:00 +0300

    change shifting letters implementation

--- leet-code/shifting-letters.py
+++ leet-code/shifting-letters.py
@@ -1,11 +1,15 @@
 from typing import List
 
 
+def shift_letter(c: str, times) -> str:
+    index = ord(c) - ord('a')
+    return chr(ord('a') + (index + times) % 26)
+
+
 def shifting_letters_for(s: str, shifts: List[int]) -> str:
     result = []
     times = sum(shifts) % 26
     for i, c in enumerate(s):
-        index = ord(c) - ord('a')
-        result.append(chr(ord('a') + (index + times) % 26))
+        result.append(shift_letter(c, times))
         times = (times - shifts[i]) % 26
     return ''.join(result)
