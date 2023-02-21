1a234b255bdc2153b80d3dd6ea5c9fc437c6fe24
yaki <kiwidancebad@gmail.com>
2023-02-21 21:19:34 +0300

    add can place flowers implementation

---
+++ leet-code/can-place-flowers.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def can_place_flowers(flowerbed: List[int], n: int) -> bool:
+    for i in range(len(flowerbed)):
+        if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+            flowerbed[i] = 1
+            n -= 1
+        if n <= 0:
+            return True
+    return False
