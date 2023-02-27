755a66ca49c5639c7ecbaf73c93d6949a6782d35
yaki <kiwidancebad@gmail.com>
2023-02-27 08:06:51 +0300

    aoc-2021-2

---
+++ aoc/2021/2-dive/2-1.py
@@ -0,0 +1,16 @@
+with open('./input', 'r', encoding='utf-8') as input_data:
+    data = input_data.read().splitlines()
+    data = [r.split() for r in data]
+    commands = [(r[0], int(r[1])) for r in data]
+    h, d = 0, 0
+    for c, v in commands:
+        if c == 'forward':
+            h += v
+        elif c == 'down':
+            d += v
+        elif c == 'up':
+            d -= v
+        else:
+            raise ValueError('Invalid command.')
+
+    print(h * d)
