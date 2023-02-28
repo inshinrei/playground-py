8826634c6bf158de5a1183ef85d060cea719684a
yaki <kiwidancebad@gmail.com>
2023-02-28 12:46:12 +0300

    aoc-2021-5

---
+++ aoc/2021/5-hydrothermal-venture/5-1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+with open('./input.txt', encoding='utf-8') as input_data:
+    data = input_data.read().splitlines()
+    data = [r.replace(' -> ', ',').split(',') for r in data]
+    data = [list(map(int, r)) for r in data]
+    size = max(max(r) for r in data) + 1
+    diagram = np.zeros((size, size), dtype=int)
+    for x1, y1, x2, y2 in data:
+        x1, y1, x2, y2 = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
+        if x1 == x2 or y1 == y2:
+            diagram[y1: y2 + 1, x1: x2 + 1] += 1
+    print(np.sum(diagram > 1))
