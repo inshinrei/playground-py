6c4bd662c77a251f01adfaae6e8d8c229953410a
yaki <kiwidancebad@gmail.com>
2023-03-01 11:13:29 +0300

    aoc-2021-10

---
+++ aoc/2021/10-syntax-scoring/10-1.py
@@ -0,0 +1,23 @@
+def count_points(line: str) -> int:
+    s = []
+    for c in line:
+        if c in '([{<':
+            s.append(c)
+        elif c in ')':
+            if s.pop() != '(':
+                return 3
+        elif c in ']':
+            if s.pop() != '[':
+                return 57
+        elif c in '}':
+            if s.pop() != '{':
+                return 1197
+        elif c in '>':
+            if s.pop() != '<':
+                return 25137
+    return 0
+
+
+with open('./input', 'r', encoding='utf-8') as input_data:
+    data = input_data.read().splitlines()
+    print(sum(count_points(r) for r in data))
