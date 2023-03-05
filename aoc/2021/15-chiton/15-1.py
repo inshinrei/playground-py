91d3e83562d9e5e5cf15181d764e7ec26f08b0a3
yaki <kiwidancebad@gmail.com>
2023-03-05 22:23:35 +0300

    aoc-2021-15

---
+++ aoc/2021/15-chiton/15-1.py
@@ -0,0 +1,9 @@
+import networkx as nx
+
+with open('./input', 'r', encoding='utf-8') as input_data:
+    data = input_data.read()
+    df = [[int(x) for x in line] for line in data.splitlines()]
+    w, h = len(df), len(df[0])
+    G = nx.grid_2d_graph(w, h, create_using=nx.DiGraph())
+    nx.set_edge_attributes(G, {e: df[e[1][0]][e[1][1]] for e in G.edges()}, 'cost')
+    print(nx.shortest_path_length(G, source=(0, 0), target=(w - 1, h - 1), weight='cost'))
