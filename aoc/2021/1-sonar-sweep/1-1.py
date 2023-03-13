f7c1f0c3867fac17c7ba847d07eb59532fde7332
yaki <kiwidancebad@gmail.com>
2023-03-13 21:26:34 +0300

    add an assertion

--- aoc/2021/1-sonar-sweep/1-1.py
+++ aoc/2021/1-sonar-sweep/1-1.py
@@ -2,4 +2,4 @@ with open('./input', 'r', encoding='utf-8') as input_data:
     data = input_data.read().splitlines()
     depths = [int(r) for r in data]
     increases = sum(x < y for x, y in zip(depths, depths[1:]))
-    print(increases)
+    assert increases == 1342
