3a877c4e66bb18f28d201ba1a0728564ac659f53
tokkiumai <kiwidancebad@gmail.com>
2023-05-12 00:30:01 +0300

    np-23-m12

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,4 +1,14 @@
 import numpy as np
 
-g = np.arange(1, 10).reshape((3, 3))
-print(g)
+np.random.seed(0)
+
+
+def compute_reciprocals(values):
+    output = np.empty(len(values))
+    for i in range(len(values)):
+        output[i] = 1.0 / values[i]
+    return output
+
+
+vs = np.random.randint(1, 10, size=5)
+print(compute_reciprocals(vs))
