f511570eddb1573c1516378a34afe83c3d4f6073
tokkiumai <kiwidancebad@gmail.com>
2023-05-12 00:27:22 +0300

    np-m12-23

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,9 +1,4 @@
 import numpy as np
 
-np.random.seed(0)
-x1 = np.random.randint(10, size=6)
-x2 = np.random.randint(10, size=(3, 4))
-x3 = np.random.randint(10, size=(3, 4, 5))
-print(x1)
-print(x2)
-print(x3)
+g = np.arange(1, 10).reshape((3, 3))
+print(g)
