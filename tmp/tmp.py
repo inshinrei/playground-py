390cc30238137685d949e3437766e901b3d40a3f
tokkiumai <kiwidancebad@gmail.com>
2023-05-24 21:50:08 +0300

    np-m24-23

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,9 +1,1 @@
-import matplotlib.pyplot as plt
-import numpy as np
 
-x = np.linspace(0, 5, 50)
-y = np.linspace(0, 5, 50)[:, np.newaxis]
-z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
-plt.imshow(z, origin='lower', extent=[0, 5, 0, 5], cmap='viridis')
-plt.colorbar()
-plt.show()
