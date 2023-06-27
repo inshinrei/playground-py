50438b68d394018d259a67187746521ff17bcae9
tokkiumai <kiwidancebad@gmail.com>
2023-06-27 17:32:18 +0300

    02.08 sorts

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -9,3 +9,12 @@ def selection_sort(x):
 
 
 print(selection_sort(np.array([2, 1, 4, 3, 5])))
+
+
+def bogosort(x):
+    while np.any(x[:-1] > x[1:]):
+        np.random.shuffle(x)
+    return x
+
+
+print(bogosort(np.array([2, 1, 4, 3, 5])))
