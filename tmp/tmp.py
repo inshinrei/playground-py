b0a95733029abea471a0ddabdf0b07f17d1a094c
tokkiumai <kiwidancebad@gmail.com>
2023-06-27 17:59:46 +0300

    pfc2

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+name = ['Alice', 'Bob', 'Cathy', 'Doug']
+age = [25, 45, 37, 19]
+weight = [55.0, 85.5, 68.0, 61.5]
+
+data = np.zeros(4, dtype={'names': ('name', 'age', 'weight'),
+                          'formats': ('U10', 'i4', 'f8')})
+print(data.dtype)
