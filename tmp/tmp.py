8eaf06cff19f608339b3bc7feb5e276fc0ae3d78
tokkiumai <kiwidancebad@gmail.com>
2023-06-27 17:30:14 +0300

    02.08 sorts

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,1 +1,11 @@
+import numpy as np
 
+
+def selection_sort(x):
+    for i in range(len(x)):
+        swap = i + np.argmin(x[i:])
+        (x[i], x[swap]) = (x[swap], x[i])
+    return x
+
+
+print(selection_sort(np.array([2, 1, 4, 3, 5])))
