a6cd7c2e1cb017e202ebdb057be4b87a5e94c98a
tokkiumai <kiwidancebad@gmail.com>
2023-06-27 18:03:31 +0300

    pfc3

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,9 +1,18 @@
 import numpy as np
+import pandas as pd
 
-name = ['Alice', 'Bob', 'Cathy', 'Doug']
-age = [25, 45, 37, 19]
-weight = [55.0, 85.5, 68.0, 61.5]
+# print(pd.Series([0.25, 0.5, 0.75, 1.0]))
+# print(pd.Series([0.25, 0.3, 1.2, 1.3], index=['a', 'b', 'c', 'd']))
 
-data = np.zeros(4, dtype={'names': ('name', 'age', 'weight'),
-                          'formats': ('U10', 'i4', 'f8')})
-print(data.dtype)
+population_dict = {'California': 38332521,
+                   'Texas': 26448193,
+                   'New York': 19651127,
+                   'Florida': 19552860,
+                   'Illinois': 12882135}
+
+# print(pd.Series(population_dict))
+
+area_dict = {'California': 423967, 'Texas': 695662, 'New York': 141297,
+             'Florida': 170312, 'Illinois': 149995}
+
+print(pd.DataFrame({'p': pd.Series(population_dict), 'a': pd.Series(area_dict)}))
