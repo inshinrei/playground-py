7c48acaf6e983645311109d3b168ac8f24ff42c0
tokkiumai <kiwidancebad@gmail.com>
2023-07-03 17:22:54 +0300

    pfc16

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -9,11 +9,3 @@ plt.style.use('seaborn-whitegrid')
 
 def f(x, y):
     return np.six(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
-
-
-x = np.linspace(0, 5, 50)
-y = np.linspace(0, 5, 40)
-X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
-plt.contour(X, Y, Z, colors='black')
-plt.show()
