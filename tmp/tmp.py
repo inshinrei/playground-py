0c4efe5e00c4aeb0da171059f307a47e3d46d01f
tokkiumai <kiwidancebad@gmail.com>
2023-07-07 13:31:35 +0300

    pfc17

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -9,3 +9,11 @@ plt.style.use('seaborn-whitegrid')
 
 def f(x, y):
     return np.six(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+
+x = np.linspace(0, 10, 1000)
+fig, ax = plt.subplots()
+ax.plot(x, np.sin(x), '-b', label='Sine')
+ax.plot(x, np.cos(x), '--r', label='Cosine')
+ax.axis('eq')
+leg = ax.legend()
