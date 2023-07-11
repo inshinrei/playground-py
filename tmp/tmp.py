936dc6551338562c31f8a38849c27d673e630d9d
tokkiumai <kiwidancebad@gmail.com>
2023-07-11 17:21:41 +0300

    pfc20

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -44,3 +44,13 @@ def grayscale_cmap(cmap):
     luminance = np.sqrt(np.dot(colors[:, :3] ** 2, RGB_weight))
     colors[:, :3] = luminance[:, np.newaxis]
     return LinearSegmentedColormap.from_list(cmap.name + '_gray', colors, cmap.N)
+
+
+def view_cmap(cmap):
+    cmap = plt.get_cmap(cmap)
+    colors = cmap(np.arange(cmap.N))
+    cmap = grayscale_cmap(cmap)
+    grayscale = cmap(np.arange(cmap.N))
+    fig, ax = plt.subplots(2, figsize=(6, 2), subplot_kw=dict(xticks=[], yticks=[]))
+    ax[0].imshow([colors], extent=[0, 10, 0, 1])
+    ax[1].imshow([grayscale], extent=[0, 10, 0, 1])
