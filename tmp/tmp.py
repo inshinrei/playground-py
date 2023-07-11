9c7c0ac18b5b335d8c8b52b0739ce30f7c7cd41d
tokkiumai <kiwidancebad@gmail.com>
2023-07-12 01:04:08 +0300

    pfc22

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -5,69 +5,10 @@ from matplotlib.colors import LinearSegmentedColormap
 import matplotlib as mpl
 import seaborn as sns
 
-plt.style.use('seaborn-whitegrid')
+births = pd.read_csv('mocks/births.csv')
+quartiles = np.percentile(births['births'], [25, 50, 75])
+mu, sig = quartiles[1], 0.74 * (quartiles[2] - quartiles[0])
+births = births.query('(births > @mu - 5 * @sig) & (births < @mu + 5 * @sig)')
 
-
-def f(x, y):
-    return np.six(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
-
-
-x = np.linspace(0, 10, 1000)
-fig, ax = plt.subplots()
-ax.plot(x, np.sin(x), '-b', label='Sine')
-ax.plot(x, np.cos(x), '--r', label='Cosine')
-ax.axis('eq')
-leg = ax.legend()
-
-cities = pd.read_csv('mocks/california_cities.csv')
-
-lat, lon = cities['latd'], cities['longd']
-population, area = cities['population_total'], cities['area_total_km2']
-
-plt.scatter(lon, lat, label=None, c=np.log10(population), cmap='viridis', s=area, linewidth=0, alpha=0.5)
-plt.axis(aspect='equal')
-plt.xlabel('long')
-plt.ylabel('lat')
-plt.colorbar(label='log$_{10}$(population)')
-plt.clim(3, 7)
-
-for area in [10, 300, 500]:
-    plt.scatter([], [], c='k', alpha=0.3, s=area, label=str(area) + ' km$^2$')
-plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='City Area')
-plt.show()
-
-
-def grayscale_cmap(cmap):
-    cmap = plt.cm.get_cmap(cmap)
-    colors = cmap(np.arange(cmap.N))
-    RGB_weight = [0.299, 0.587, 0.114]
-    luminance = np.sqrt(np.dot(colors[:, :3] ** 2, RGB_weight))
-    colors[:, :3] = luminance[:, np.newaxis]
-    return LinearSegmentedColormap.from_list(cmap.name + '_gray', colors, cmap.N)
-
-
-def view_cmap(cmap):
-    cmap = plt.get_cmap(cmap)
-    colors = cmap(np.arange(cmap.N))
-    cmap = grayscale_cmap(cmap)
-    grayscale = cmap(np.arange(cmap.N))
-    fig, ax = plt.subplots(2, figsize=(6, 2), subplot_kw=dict(xticks=[], yticks=[]))
-    ax[0].imshow([colors], extent=[0, 10, 0, 1])
-    ax[1].imshow([grayscale], extent=[0, 10, 0, 1])
-
-
-fig0 = plt.figure()
-fig0.subplots_adjust(hspace=0.4, wspace=0.4)
-for i in range(1, 7):
-    ax = fig0.add_subplot(2, 3, i)
-    ax.text(0.5, 0.5, str((2, 3, i)), fontsize=18, ha='center')
-
-mean = [0, 0]
-cov = [[1, 1], [1, 2]]
-x, y = np.random.multivariate_normal(mean, cov, 3000).T
-
-fig1 = plt.figure(figsize=(6, 6))
-grid = plt.GridSpec(4, 4, hspace=0.2, wspace=0.2)
-main_ax = fig1.add_subplot(grid[:-1, 1:])
-y_hist = fig1.add_subplot(grid[:-1, 0], xticklabels=[], sharey=main_ax)
-x_hist = fig1.add_subplot(grid[-1, 1:], yticklabels=[], sharex=main_ax)
+births['day'] = births['day'].astype(int)
+births.index = pd.to_datetime(10000 * births.year + 100 * births.month + births.day, format='%Y%m%d')
