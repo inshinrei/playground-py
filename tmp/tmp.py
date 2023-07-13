2b318d74398849a060267dc8825d7150194b580a
tokkiumai <kiwidancebad@gmail.com>
2023-07-13 22:20:31 +0300

    pfc23

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -4,11 +4,3 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib as mpl
 import seaborn as sns
-
-births = pd.read_csv('mocks/births.csv')
-quartiles = np.percentile(births['births'], [25, 50, 75])
-mu, sig = quartiles[1], 0.74 * (quartiles[2] - quartiles[0])
-births = births.query('(births > @mu - 5 * @sig) & (births < @mu + 5 * @sig)')
-
-births['day'] = births['day'].astype(int)
-births.index = pd.to_datetime(10000 * births.year + 100 * births.month + births.day, format='%Y%m%d')
