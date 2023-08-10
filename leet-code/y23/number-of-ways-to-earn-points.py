d9ff3ae2e8423b368be4cdeded36a9cc42653ec8
tokkiumai <kiwidancebad@gmail.com>
2023-08-10 15:05:18 +0300

    add number-of-ways-to-earn-points.py

---
+++ leet-code/y23/number-of-ways-to-earn-points.py
@@ -0,0 +1,14 @@
+def ways_to_reach_target(target, types):
+    k_mod = 1_000_000_007
+    dp = [1] + [0] * target
+    for count, mark in types:
+        for j in range(target, -1, -1):
+            for solved in range(1, count + 1):
+                if j - solved * mark >= 0:
+                    dp[j] += dp[j - solved * mark]
+                    dp[j] %= k_mod
+    return dp[target]
+
+
+assert ways_to_reach_target(6, [[6, 1], [3, 2], [2, 3]]) == 7
+assert ways_to_reach_target(5, [[50, 1], [50, 2], [50, 5]]) == 4
