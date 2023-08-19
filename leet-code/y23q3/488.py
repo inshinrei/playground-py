7a874d1bac5688d0bb6d94c8d5afe3a75e35560e
tokkiumai <kiwidancebad@gmail.com>
2023-08-19 19:26:13 +0300

    add 488.py

---
+++ leet-code/y23q3/488.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+import math
+
+
+def find(board, hand):
+    def de(_board):
+        start = 0
+        for i, c in enumerate(_board):
+            if c != _board[start]:
+                if i - start >= 3:
+                    return de(_board[:start] + _board[i:])
+            start = i
+        return _board
+
+    @lru_cache(None)
+    def dfs(_board, _hand):
+        _board = de(_board)
+        if _board == '#':
+            return 0
+        board_set = set(_board)
+        _hand = ''.join(h for h in _hand if h in board_set)
+        result = math.inf
+        if not _hand:
+            return result
+        for i in range(len(_board)):
+            for j, h in enumerate(_hand):
+                new_hand = _hand[:j] + _hand[j + 1:]
+                new_board = _board[:i] + h + _board[i:]
+                result = min(result, 1 + dfs(new_board, new_hand))
+        return result
+
+    result = dfs(board + '#', hand)
+    return -1 if result == math.inf else result
