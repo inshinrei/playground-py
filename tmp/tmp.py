3106b48288c684ca3ad56361e7aefc947fcddf64
tokkiumai <kiwidancebad@gmail.com>
2023-08-21 05:52:30 +0300

    tmp 3-d plotting

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -6,22 +6,19 @@ import matplotlib as mpl
 import seaborn as sns
 from sklearn.datasets import fetch_olivetti_faces
 
-ax = plt.axes(axisbg='#e6e6e6')
-ax.set_axisbelow(True)
-plt.grid(color='w', linestyle='solid')
-for s in ax.spines.values():
-    s.set_visible(False)
-ax.xaxis.tick_bottom()
-ax.yaxis.tick_left()
-ax.tick_params(colors='gray', direction='out')
-for tick in ax.get_xticklabels():
-    tick.set_color('gray')
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+zline = np.linspace(0, 15, 1000)
+xline = np.sin(zline)
+yline = np.cos(zline)
+ax.plot3D(xline, yline, zline, 'gray')
+zdata = 15 * np.random.random(100)
+xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
 
-
-def hist_and_lines():
-    np.random.seed(0)
-    fig, ax = plt.subplots(1, 2, figsize=(11, 4))
-    ax[0].hist(np.random.randn(1000))
-    for i in range(3):
-        ax[1].plot(np.random.rand(10))
-    ax[1].legend(['a', 'b', 'c'], loc='lower left')
+r = np.linspace(0, 6, 20)
+theta = np.linspace(-0.9 * np.pi, 0.8 * np.pi, 40)
+r, theta = np.meshgrid(r, theta)
+X = r * np.sin(theta)
+Y = r * np.cos(theta)
