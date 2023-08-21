9f1bef609b49727a33a0ac897c761fde35392570
tokkiumai <kiwidancebad@gmail.com>
2023-08-21 05:44:49 +0300

    ticks tmp

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -4,3 +4,19 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib as mpl
 import seaborn as sns
+from sklearn.datasets import fetch_olivetti_faces
+
+plt.style.use('classic')
+ax = plt.axes(xcale='log', yscale='log')
+ax.grid()
+ax.yaxis.set_major_locator(plt.NullLocator())
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+
+fig, ax = plt.subplots(5, 5, figsize=(5, 5))
+fig.subplots_adjust(hspace=0, wspace=0)
+faces = fetch_olivetti_faces().images
+for i in range(5):
+    for j in range(5):
+        ax[i, j].xaxis.set_major_loactor(plt.NullLocator())
+        ax[i, j].yaxis.set_major_locator(plt.NullLocator())
+        ax[i, j].imshow(faces[10 * i + j], cmap='bone')
