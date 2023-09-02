9f01e56124e371f25bf3522cae8d31da4b41febf
tokkiumai <kiwidancebad@gmail.com>
2023-09-02 23:16:30 +0300

    bayes

--- tmp/bayes.py
+++ tmp/bayes.py
@@ -8,3 +8,15 @@ from sklearn.datasets import make_blobs
 
 X, y = make_blobs(100, 2, centers=2, random_state=2, cluster_std=1.5)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
+
+from sklearn.naive_bayes import GaussianNB
+
+model = GaussianNB()
+model.fit(X, y)
+rng = np.random.RandomState(0)
+X_new = [-6, -14] + [14, 18] * rng.rand(2000, 2)
+y_new = model.predict(X_new)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
+lim = plt.axis()
+plt.scatter(X_new[:, 0], X_new[:, 1], c=y_new, s=20, cmap='RdBu', alpha=0.1)
+plt.axis(lim)
