72a6f79434eebbc26c1e1f7c4468d4b9a2df971b
tokkiumai <kiwidancebad@gmail.com>
2023-09-02 23:12:13 +0300

    bayes

---
+++ tmp/bayes.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(100, 2, centers=2, random_state=2, cluster_std=1.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
