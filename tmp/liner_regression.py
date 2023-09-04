d59753069ab323bc9968e2d92fb8a76403cf258e
tokkiumai <kiwidancebad@gmail.com>
2023-09-04 17:28:47 +0300

    liner regression

--- tmp/liner_regression.py
+++ tmp/liner_regression.py
@@ -17,3 +17,19 @@ x_fit = np.linspace(0, 10, 1000)
 y_fit = model.predict(x_fit[:, np.newaxis])
 plt.scatter(x, y)
 plt.plot(x_fit, y_fit)
+
+from sklearn.preprocessing import PolynomialFeatures
+
+x = np.array([2, 3, 4])
+poly = PolynomialFeatures(3, include_bias=False)
+poly.fit_transform(x[:, None])
+
+from sklearn.pipeline import make_pipeline
+
+poly_model = make_pipeline(PolynomialFeatures(7), LinearRegression())
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = np.sin(x) + 0.1 * rng.randn(50)
+poly_model.fit(x[:, np.newaxis], y)
+plt.scatter(x, y)
+plt.plot(x_fit, y_fit)
