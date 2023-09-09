1a6a1480497cc017d608309cef97b3de94819ed2
tokkiumai <kiwidancebad@gmail.com>
2023-09-09 11:38:25 +0300

    liner regression

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -5,20 +5,3 @@ from matplotlib.colors import LinearSegmentedColormap
 import matplotlib as mpl
 import seaborn as sns
 from sklearn.datasets import fetch_olivetti_faces
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-zline = np.linspace(0, 15, 1000)
-xline = np.sin(zline)
-yline = np.cos(zline)
-ax.plot3D(xline, yline, zline, 'gray')
-zdata = 15 * np.random.random(100)
-xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
-
-r = np.linspace(0, 6, 20)
-theta = np.linspace(-0.9 * np.pi, 0.8 * np.pi, 40)
-r, theta = np.meshgrid(r, theta)
-X = r * np.sin(theta)
-Y = r * np.cos(theta)
