ad7e78b3b9f57107dc1702bc20c678397e32f95b
tokkiumai <kiwidancebad@gmail.com>
2023-09-11 14:45:48 +0300

    support vector machines

--- tmp/svm.py
+++ tmp/svm.py
@@ -23,3 +23,29 @@ for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
     plt.plot(x_fit, y_fit, '-k')
     plt.fill_between(x_fit, y_fit - d, y_fit + d, edgecolor='none', color='#AAAAAA', alpha=0.4)
 plt.xlim(-1, 3.5)
+
+from sklearn.svm import SVC
+
+model = SVC(kernel='linear', C=1E10)
+model.fit(X, y)
+
+
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    if ax is None:
+        ax = plt.gca()
+    x_lim = ax.get_xlim()
+    y_lim = ax.get_ylim()
+    x = np.linspace(x_lim[0], x_lim[1], 30)
+    y = np.linspace(y_lim[0], y_lim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    ax.contour(X, Y, P, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1], s=300, linewidth=1, facecolors='none')
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
+
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(model)
