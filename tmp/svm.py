506fdbcac44f66eabbc3f854f84bcb72560b8ca8
tokkiumai <kiwidancebad@gmail.com>
2023-09-11 14:39:52 +0300

    support vector machines

---
+++ tmp/svm.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+from sklearn.datasets._samples_generator import make_blobs
+
+X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.60)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+
+x_fit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
+for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
+    plt.plot(x_fit, m * x_fit + b, '-k')
+plt.xlim(-1, 3.5)
