3eec340bf83215b98016eba77dab86e5b143c24c
tokkiumai <kiwidancebad@gmail.com>
2023-09-11 14:41:37 +0300

    support vector machines

--- tmp/svm.py
+++ tmp/svm.py
@@ -15,3 +15,11 @@ plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
 for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
     plt.plot(x_fit, m * x_fit + b, '-k')
 plt.xlim(-1, 3.5)
+
+x_fit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
+    y_fit = m * x_fit + b
+    plt.plot(x_fit, y_fit, '-k')
+    plt.fill_between(x_fit, y_fit - d, y_fit + d, edgecolor='none', color='#AAAAAA', alpha=0.4)
+plt.xlim(-1, 3.5)
