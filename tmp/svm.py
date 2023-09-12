55bd712dca0f05df8a71330e741b679e4f4f26a8
tokkiumai <kiwidancebad@gmail.com>
2023-09-12 18:11:11 +0300

    support vector machines

--- tmp/svm.py
+++ tmp/svm.py
@@ -49,3 +49,23 @@ def plot_svc_decision_function(model, ax=None, plot_support=True):
 
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 plot_svc_decision_function(model)
+
+
+def plot_svm(N=10, ax=None):
+    X, y = make_blobs(n_samples=200, centers=2, random_state=0, cluster_std=0.60)
+    X = X[:N]
+    y = y[:N]
+    model = SVC(kernel='liner', C=1E10)
+    model.fit(X, y)
+    ax = ax or plt.gca()
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    ax.set_xlim(-1, 4)
+    ax.set_ylim(-1, 6)
+    plot_svc_decision_function(model, ax)
+
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
+for axi, N in zip(ax, [60, 120]):
+    plot_svm(N, axi)
+    axi.set_title('N = {0}'.format(N))
