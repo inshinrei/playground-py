71f3193c1074f5b28bb4611bb9f908ab8ba5e54a
tokkiumai <kiwidancebad@gmail.com>
2023-09-19 13:37:48 +0300

    random forests

--- tmp/random_forests.py
+++ tmp/random_forests.py
@@ -31,3 +31,37 @@ def visualize_classifier(model, X, y, ax=None, cmap='rainbow'):
 
 
 visualize_classifier(DecisionTreeClassifier(), X, y)
+
+from sklearn.ensemble import BaggingClassifier
+
+tree = DecisionTreeClassifier()
+bag = BaggingClassifier(tree, n_estimators=100, max_samples=0.8, random_state=1)
+bag.fit(X, y)
+visualize_classifier(bag, X, y)
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+visualize_classifier(model, X, y)
+
+rng = np.random.RandomState(42)
+x = 10 * rng.rand(200)
+
+
+def get_model(x, sigma=0.3):
+    fast_oscillation = np.sin(5 * x)
+    slow_oscillation = np.sin(0.5 * x)
+    noise = sigma * rng.randn(len(x))
+    return slow_oscillation + fast_oscillation + noise
+
+
+from sklearn.ensemble import RandomForestRegressor
+
+forest = RandomForestRegressor(200)
+forest.fit(x[:, None], y)
+x_fit = np.linspace(0, 10, 1000)
+y_fit = forest.predict(x_fit[:, None])
+y_true = get_model(x_fit, sigmna=0)
+plt.errorbar(x, y, 0.3, fmt='o', alpha=0.5)
+plt.plot(x_fit, y_fit, '-r')
+plt.plot(x_fit, y_true, '-k', alpha=0.5)
