e9c66f8b17eb97e23df0eac587f49df8ad616408
tokkiumai <kiwidancebad@gmail.com>
2023-09-24 13:16:20 +0300

    principal component analysis

--- tmp/pca.py
+++ tmp/pca.py
@@ -7,3 +7,14 @@ sns.set()
 rng = np.random.RandomState(1)
 X = np.dot(rng.rand(2, 2), rng.randn(2, 200)).T
 plt.scatter(X[:, 0], X[:, 1])
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+pca.fit(X)
+
+
+def draw_vector(v0, v1, ax=None):
+    ax = ax or plt.gca()
+    arrow_props = dict(arrowstyle='->', linewidth=2, shrinkA=0, shrinkB=0)
+    ax.annotate('', v1, v0, arrowprops=arrow_props)
