b4e984761987ab1c9e12c2d1a0a0a86d75de91a7
tokkiumai <kiwidancebad@gmail.com>
2023-09-26 15:40:30 +0300

    principal component analysis

--- tmp/pca.py
+++ tmp/pca.py
@@ -38,3 +38,13 @@ pca = PCA(2)
 projected = pca.fit_transform(digits.data)
 plt.scatter(projected[:, 0], projected[:, 1], c=digits.target, edgecolor='none', alpha=0.5,
             cmap=plt.cm.get_cmap('spectral', 10))
+
+
+def plot_digits(data):
+    fig, axes = plt.subplots(4, 10, figsize=(10, 4), subplot_kw={'xticks': [], 'yticks': []},
+                             gridspec_kw=dict(hspace=0.1, wspace=0.1))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(data[i].reshape(8, 8), cmap='binary', interpolation='nearest', clim=(0, 16))
+
+
+plot_digits(digits.data)
