2c5f203d2e3ec6dd7e308e304f6d2ee88bc72366
tokkiumai <kiwidancebad@gmail.com>
2023-10-09 13:43:49 +0300

    gaussian mixture

--- tmp/gaussian_mixture.py
+++ tmp/gaussian_mixture.py
@@ -13,3 +13,20 @@ from sklearn.cluster import KMeans
 kmeans = KMeans(4, random_state=0)
 labels = kmeans.fit(X).predict(X)
 plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis')
+
+from scipy.spatial.distance import cdist
+
+
+def plot_kmeans(_kmeans, _X, ax=None):
+    _labels = _kmeans.fit_predict(_X)
+    ax = ax or plt.gca()
+    ax.axis('eq')
+    ax.scatter(_X[:, 0], _X[:, 1], c=_labels, s=40, cmap='viridis', zorder=2)
+    _centers = _kmeans.cluster_centers_
+    radii = [cdist(_X[_labels == i], [center]).max() for i, center in enumerate(_centers)]
+    for c, r in zip(_centers, radii):
+        ax.add_patch(plt.Circle(c, r, fc='#CCCCCC', lw=4, alpha=0.5, zorder=1))
+
+
+kmeans = KMeans(n_clusters=4, random_state=0)
+plot_kmeans(kmeans, X)
