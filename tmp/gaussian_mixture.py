f67e474ef2e0a8317b7b87616783b8cce7728dc4
tokkiumai <kiwidancebad@gmail.com>
2023-10-14 23:24:41 +0300

    gaussian mixture

--- tmp/gaussian_mixture.py
+++ tmp/gaussian_mixture.py
@@ -35,3 +35,44 @@ rng = np.random.RandomState(13)
 X_stretched = np.dot(X, rng.randn(2, 2))
 kmeans = KMeans(n_clusters=4, random_state=0)
 plot_kmeans(kmeans, X_stretched)
+
+from sklearn.mixture import GaussianMixture
+
+gmm = GaussianMixture(n_components=4).fit(X)
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis')
+probs = gmm.predict_proba(X)
+size = 50 * probs.max(1) ** 2
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', s=size)
+
+from matplotlib.patches import Ellipse
+
+
+def draw_ellipse(position, covariance, ax=None, **kwargs):
+    ax = ax or plt.gca()
+    if covariance.shape == (2, 2):
+        U, s, Vt = np.linalg.svd(covariance)
+        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width, height = 2 * np.sqrt(s)
+    else:
+        angle = 0
+        width, height = 2 * np.sqrt(covariance)
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position, nsig * width, nsig * height, angle, **kwargs))
+
+
+def plot_gmm(gmm, _X, label=True, ax=None):
+    ax = ax or plt.gca()
+    _labels = gmm.ftt(_X).predict(_X)
+    if label:
+        ax.scatter(_X[:, 0], _X[:, 1], c=_labels, s=40, cmap='viridis', zorder=2)
+    else:
+        ax.scatter(_X[:, 0], _X[:, 1], s=40, zorder=2)
+    ax.axis('equal')
+    w_factor = 0.2 / gmm.weights_.max()
+    for pos, covar, w in zip(gmm.means_, gmm.covars_, gmm.weights_):
+        draw_ellipse(pos, covar, alpha=w * w_factor)
+
+
+gmm = GaussianMixture(n_components=4, random_state=42)
+plot_gmm(gmm, X)
