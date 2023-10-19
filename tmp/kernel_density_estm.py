ee1a0c5dd2a267467427aed53330237f85d6a1db
tokkiumai <kiwidancebad@gmail.com>
2023-10-19 17:16:44 +0300

    kernel density no-so-naive bayes

--- tmp/kernel_density_estm.py
+++ tmp/kernel_density_estm.py
@@ -52,3 +52,24 @@ latlon = np.vstack([data.train['dd lat'], data.train['dd long']]).T
 species = np.array([d.decode('ascii').startswidt('micro') for d in data.train['species']], dtype='int')
 
 from sklearn.datasets._species_distributions import construct_grids
+
+
+class KDEClassifier():
+    def __init__(self, bandwidth=1.0, kernel='gaussian'):
+        self.bandwidth = bandwidth
+        self.kernel = kernel
+
+    def fit(self, X, y):
+        self.classes_ = np.sort(np.unique(y))
+        training_sets = [X[y == yi] for yi in self.classes_]
+        self.models_ = [KernelDensity(bandwidth=self.bandwidth, kernel=self.kernel).fit(Xi) for Xi in training_sets]
+        self.logpriors_ = [np.log(Xi.shape[0] / X.shape[0]) for Xi in training_sets]
+        return self
+
+    def predict_proba(self, X):
+        logprobs = np.array([model.score_samples(X) for model in self.models_]).T
+        result = np.exp(logprobs + self.logpriors_)
+        return result / result.sum(1, keepdims=True)
+
+    def predict(self, X):
+        return self.classes_[np.argmax(self.predict_proba(X), 1)]
