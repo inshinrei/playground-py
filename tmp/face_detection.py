b6481de0638d55d87324f384a6f4ac89ce07e016
tokkiumai <kiwidancebad@gmail.com>
2023-10-19 17:27:35 +0300

    a face detection pipeline - obtain a set of positive samples

--- tmp/face_detection.py
+++ tmp/face_detection.py
@@ -16,3 +16,32 @@ ax[1].imshow(hog_vis)
 ax[1].set_title('HOG features')
 
 from sklearn.datasets import fetch_lfw_people
+
+faces = fetch_lfw_people()
+positive_patches = faces.images
+
+from skimage import transform
+
+imgs_to_use = ['camera', 'text', 'coins', 'moon', 'page', 'clock', 'immunohistochemistry',
+               'chelsea', 'coffee', 'hubble_deep_field']
+images = [color.rgb2gray(getattr(data, name)()) for name in imgs_to_use]
+
+from sklearn.feature_extraction.image import PatchExtractor
+
+
+def extract_patches(img, N, scale=1.0, patch_size=positive_patches[0].shape):
+    extracted_patch_size = tuple((scale * np.array(patch_size)).astype(int))
+    extractor = PatchExtractor(patch_size=extracted_patch_size, max_patches=N, random_state=0)
+    patches = extractor.transform(img[np.newaxis])
+    if scale != 1:
+        patches = np.array([transform.resize(patch, patch_size) for patch in patches])
+    return patches
+
+
+negative_patches = np.vstack([extract_patches(im, 1000, scale) for im in images for scale in [0.5, 1.0, 2.0]])
+fig, ax = plt.subplots(6, 10)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(negative_patches[500 * i], cmap='gray')
+    axi.axis('off')
+
+from itertools import chain
