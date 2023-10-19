c8d4b17e06f826a99b71b4050eea84eaeabaf405
tokkiumai <kiwidancebad@gmail.com>
2023-10-19 16:51:58 +0300

    kernel density

--- tmp/kernel_density_estm.py
+++ tmp/kernel_density_estm.py
@@ -3,3 +3,37 @@ import seaborn
 import numpy as np
 
 seaborn.set()
+
+
+def make_data(_N, _f=0.3, _seed=1):
+    _X = np.random.RandomState(_seed).randn(_N)
+    _X[int(_f * _N):] += 5
+    return _X
+
+
+x = make_data(1000)
+hist = plt.hist(x, bins=30, normed=True)
+x = make_data(20)
+bins = np.linspace(-5, 10, 10)
+fig, ax = plt.subplots(1, 1, figsize=(12, 4), sharex=True, sharey=True,
+                       subplot_kw={'xlim': (-4, 9), 'ylim': (-0.02, 0.3)})
+fig.subplots_adjust(wspace=0.05)
+for i, offset in enumerate([0.0, 0.6]):
+    ax[i].hist(x, bins=bins + offset, normed=True)
+    ax[i].plot(x, np.full_like(x, -0.01), '|k', markeredgewidth=1)
+
+fig, ax = plt.subplots()
+bins = np.arange(-3, 8)
+ax.plot(x, np.full_like(x, -0.1), '|k', markeredgewidth=1)
+for count, edge in zip(*np.histogram(x, bins)):
+    for i in range(count):
+        ax.add_patch(plt.Rectangle((edge, i), 1, 1, alpha=0.5))
+
+x_d = np.linspace(-4, 8, 2000)
+density = sum((abs(xi - x_d) < 0.5) for xi in x)
+plt.fill_between(x_d, density, alpha=0.5)
+
+from scipy.stats import norm
+
+x_d = np.linspace(-4, 8, 1000)
+density = sum(norm(xi).pdf(x_d) for xi in x)
