ec8d603dd4e3266f7be402c9581786464679d436
tokkiumai <kiwidancebad@gmail.com>
2023-11-05 23:43:37 +0300

    add bfs example

--- lrn/algorithms/graphs/bfs.py
+++ lrn/algorithms/graphs/bfs.py
@@ -19,3 +19,31 @@ def flood_fill(img, row, col, p):
             if (row, col) not in visited:
                 queue.append((row, col))
     return img
+
+
+from graphframes import *
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+import pandas as pd
+
+spark = SparkSession()
+
+fields = [
+    StructField('id', StringType(), True),
+    StructField('latitude', FloatType(), True),
+    StructField('longitude', FloatType(), True),
+    StructField('population', IntegerType(), True)
+]
+v = spark.read.csv('data/transport-nodes.csv', header=True, schema=StructType(fields))
+src_dst = spark.read.csv('data/transport-relationships.csv', header=True)
+df_src_dst = src_dst.toPandas()
+df_dst_src = src_dst.toPandas()
+df_dst_src.columns = ['dst', 'src', 'relationship', 'cost']
+e = spark.createDataFrame(pd.concat([df_src_dst, df_dst_src]))
+g = GraphFrame(v, e)
+(g.vertices.filter('population > 100000 and population < 300000').sort('population').show())
+from_expr = "id='Den Haag'"
+to_expr = "population > 100000 and population < 300000 and id <> 'Den Haag'"
+result = g.bfs(from_expr, to_expr)
+columns = [column for column in result.columns if not column.startswith('e')]
+result.select(columns).show()
