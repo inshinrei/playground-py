952da24b93208cacce757390d6daabe76516449e
tokkiumai <kiwidancebad@gmail.com>
2023-11-05 23:44:12 +0300

    remove tmp

--- tmp/tmp.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
-import matplotlib as mpl
-import seaborn as sns
-from sklearn.datasets import fetch_olivetti_faces
