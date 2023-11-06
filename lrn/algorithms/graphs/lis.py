113cbdb2cfefbaa69b2d28a937fe926b153e33a0
tokkiumai <kiwidancebad@gmail.com>
2023-11-06 11:41:40 +0300

    add LIS implementation

---
+++ lrn/algorithms/graphs/lis.py
@@ -0,0 +1,6 @@
+def lis(A):
+    L = [1] * len(A)
+    for i in range(1, len(L)):
+        sub_problems = [L[k] for k in range(i) if A[k] < A[i]]
+        L[i] = 1 + max(sub_problems, default=0)
+    return max(L, default=0)
