0e205feb51c8835b9a9ecd6d3bf8bbd0a3618f06
tokkiumai <kiwidancebad@gmail.com>
2023-11-29 21:45:55 +0300

    add AggregateMessages class

--- lrn/algorithms/graphs/aggregate_m.py
+++ lrn/algorithms/graphs/aggregate_m.py
@@ -14,3 +14,32 @@ class _Class_Property(object):
 
     def __get__(self, _, owner):
         return self.f(owner)
+
+
+class AggregateMessages(object):
+    @_Class_Property
+    def src(_):
+        jvm_gf_api = _java_api(SparkContext)
+        return f.col(jvm_gf_api.SRC())
+
+    @_Class_Property
+    def dst(_):
+        jvm_gf_api = _java_api(SparkContext)
+        return f.col(jvm_gf_api.DST())
+
+    @_Class_Property
+    def edge(_):
+        jvm_gf_api = _java_api(SparkContext)
+        return f.col(jvm_gf_api.EDGE())
+
+    @_Class_Property
+    def msg(_):
+        jvm_gf_api = _java_api(SparkContext)
+        return f.col(jvm_gf_api.aggregateMessages().MSG_COL_NAME())
+
+    @staticmethod
+    def getCachedDataFrame(df):
+        sql_context = df.sql_ctx
+        jvm_gf_api = _java_api(sql_context._sc)
+        jdf = jvm_gf_api.aggregateMessages().getCachedDataFrame(df._jdf)
+        return DataFrame(jdf, sql_context)
