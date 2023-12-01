c2c95fa94cbf458beead0c360da2de0a1d615875
tokkiumai <kiwidancebad@gmail.com>
2023-12-01 19:30:01 +0300

    add df

--- lrn/algorithms/graphs/p1ap/airport.py
+++ lrn/algorithms/graphs/p1ap/airport.py
@@ -29,3 +29,17 @@ cleaned_relationships = relationships.select('ORIGIN', 'DEST', 'FL_DATE', 'DEP_D
                                                                                                               'arrDelay').cast(
                                                                                                               FloatType())).withColumn(
     'time', f.col('time').cast(IntegerType())).withColumn('arrivalTime', f.col('arrivalTime').cast(IntegerType()))
+g = GraphFrame(cleaned_nodes, cleaned_relationships)
+airlines_reference = spark.read.csv('../data/airlines.csv').select('_c1', '_c3').withColumnRenamed('_c1',
+                                                                                                   'name').withColumnRenamed(
+    '_c3', 'code')
+airlines_reference = airlines_reference[airlines_reference['code'] != 'null']
+df = spark.read.option('multiline', 'true').json('../data/airlines.json')
+dummy_df = spark.createDataFrame([('test', 'test')], ['code', 'name'])
+for code in df.schema.fieldNames():
+    temp_df = df.withColumn('code', f.lit(code)).withColumn('name', df[code])
+    tdf = temp_df.select('code', 'name')
+    dummy_df = dummy_df.union(tdf)
+g.vertices.count()
+g.edges.count()
+g.edges.groupBy().max('deptDelay').show()
