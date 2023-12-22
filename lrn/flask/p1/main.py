367d2567e069fbf862d22596fd6b5226ce9d0fb4
tokkiumai <kiwidancebad@gmail.com>
2023-12-23 00:21:28 +0300

    add basic validator

--- lrn/flask/p1/main.py
+++ lrn/flask/p1/main.py
@@ -1,21 +1,23 @@
-from flask import Flask
-
-# from flask_script import Manager
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'str'
 
-manager = Manager(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello World!</h1>'
-
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
-
-
-if __name__ == '__main__':
-    manager.run()
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('You have changed your name')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
