aaa320e10bc05852f35dbd33c47ed1c83ab5ea40
tokkiumai <kiwidancebad@gmail.com>
2024-01-06 15:46:12 +0300

    add login and logout

--- lrn/flask/p1/app/auth/views.py
+++ lrn/flask/p1/app/auth/views.py
@@ -1,7 +1,28 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', method=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('invalid email or password')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('you have been logged out')
+    return redirect(url_for('main.index'))
