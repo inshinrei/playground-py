43d8b557a6c5637cf99e97643c6080d32a3be59d
tokkiumai <kiwidancebad@gmail.com>
2024-01-22 22:28:21 +0300

    feat: markdown for posts

--- lrn/flask/p1/app/models.py
+++ lrn/flask/p1/app/models.py
@@ -1,8 +1,11 @@
-from datetime import datetime
 import hashlib
-from flask import current_app, request
+from datetime import datetime
+
+import bleach
+from flask import current_app
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedSerializer
+from markdown import markdown
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db, login_manager
@@ -195,9 +198,20 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    @staticmethod
+    def on_changed_body(target, value):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(
+            bleach.clean(markdown(value, output_format='html'), tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 login_manager.anonymous_user = AnonymousUser
 
