b7568960a2ffaa2292c1029d4404ee01623c8a4b
tokkiumai <kiwidancebad@gmail.com>
2024-01-29 08:37:00 +0300

    feat: deploy

--- lrn/flask/p1/main.py
+++ lrn/flask/p1/main.py
@@ -9,7 +9,7 @@ if os.environ.get('FLASK_COVERAGE'):
 
 import sys
 import click
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
 
@@ -57,4 +57,11 @@ def profile(length, profile_dir):
     """Start the application under the code profiler."""
     from werkzeug.contrib.profiler import ProfilerMiddleware
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length], profile_dir=profile_dir)
-    app.run(debug=False)
+    app.run()
+
+
+@app.cli.command()
+def deploy():
+    upgrade()
+    Role.insert_roles()
+    User.add_self_follows()
