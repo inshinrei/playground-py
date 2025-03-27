5decf0aac872cb166362172bac6d90e8ab9ae756
tokkiumai <kiwidancebad@gmail.com>
2025-03-27 10:41:25 +0300

    parser options

--- projects/augmented-p1/main.py
+++ projects/augmented-p1/main.py
@@ -11,5 +11,6 @@ parser.add_argument('-mk', '--model_keypoints', help='draw model keypoints', act
 parser.add_argument('-fk', '--frame_keypoints', help='draw frame keypoints', action='store_true')
 parser.add_argument('-ma', '--matches', help='draw matches between keypoints', action='store_true')
 args = parser.parse_args()
+
 if __name__ == '__main__':
     main()
