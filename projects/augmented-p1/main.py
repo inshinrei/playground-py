71f2c0967f8ff0f510a7b7bf12739f247f08e700
tokkiumai <kiwidancebad@gmail.com>
2025-03-31 12:18:03 +0300

    obj loader

--- projects/augmented-p1/main.py
+++ projects/augmented-p1/main.py
@@ -1,15 +1,56 @@
 import argparse
+import os
+
+import cv2
+import numpy as np
+
+MIN_MATCHES = 10
+DEFAULT_COLOR = (0, 0, 0)
+
+
+class ObjLoader:
+    def __init__(self, filename, swapyz=False):
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith("#"):
+                continue
+            values = line.split()
+            if not values:
+                continue
 
 
 def main():
-    pass
+    homography = None
+    camera_params = np.array([[800, 0, 320], [0, 800, 240], [0, 0, 1]])
+    orb = cv2.ORB_create()
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    dir_name = os.getcwd()
+    model = cv2.imread(os.path.join(dir_name, "ref/model.jpg"), 0)
+    kp_model, des_model = orb.detectAndCompute(model, None)
+    obj = ObjLoader(os.path.join(dir_name, "models/fox.obj"), swapyz=True)
+    cap = cv2.VideoCapture(0)
 
 
-parser = argparse.ArgumentParser(description='app')
-parser.add_argument('-r', '--rectangle', help='draw rectangle delimiting target surface', action='store_true')
-parser.add_argument('-mk', '--model_keypoints', help='draw model keypoints', action='store_true')
-parser.add_argument('-fk', '--frame_keypoints', help='draw frame keypoints', action='store_true')
-parser.add_argument('-ma', '--matches', help='draw matches between keypoints', action='store_true')
+parser = argparse.ArgumentParser(description="app")
+parser.add_argument(
+    "-r",
+    "--rectangle",
+    help="draw rectangle delimiting target surface",
+    action="store_true",
+)
+parser.add_argument(
+    "-mk", "--model_keypoints", help="draw model keypoints", action="store_true"
+)
+parser.add_argument(
+    "-fk", "--frame_keypoints", help="draw frame keypoints", action="store_true"
+)
+parser.add_argument(
+    "-ma", "--matches", help="draw matches between keypoints", action="store_true"
+)
 args = parser.parse_args()
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
