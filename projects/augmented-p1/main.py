f3bd77aebfc8e0706035f09d8c00d4293b79694c
tokkiumai <kiwidancebad@gmail.com>
2025-04-03 12:39:03 +0300

    finalize rendering matrix

--- projects/augmented-p1/main.py
+++ projects/augmented-p1/main.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import os
 
 import cv2
@@ -61,6 +62,99 @@ def main():
     kp_model, des_model = orb.detectAndCompute(model, None)
     obj = ObjLoader(os.path.join(dir_name, "models/fox.obj"), swapyz=True)
     cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("unable to read frame")
+            return
+        kp_frame, des_frame = orb.detectAndCompute(frame, None)
+        matches = bf.match(des_model, des_frame)
+        matches = sorted(matches, key=lambda x: x.distance)
+        if len(matches) < MIN_MATCHES:
+            src_pts = np.float32([kp_model[m.queryIdx].pt for m in matches]).reshape(
+                -1, 1, 2
+            )
+            dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in matches]).reshape(
+                -1, 1, 2
+            )
+            homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            if args.rectangle:
+                h, w = model.shape
+                pts = np.float32(
+                    [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]
+                ).reshape(-1, 1, 2)
+                dst = cv2.perspectiveTransform(pts, homography)
+                frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+            if homography is not None:
+                try:
+                    projection = projection_matrix(camera_params, homography)
+                    frame = render(frame, obj, projection, model, False)
+                except:
+                    pass
+            if args.matches:
+                frame = cv2.drawMatches(
+                    model, kp_model, frame, kp_frame, matches[:10], 0, flags=2
+                )
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            print("not enough matches %d/%d" % (len(matches), MIN_MATCHES))
+    cap.release()
+    cv2.destroyAllWindows()
+    return 0
+
+
+def render(img, obj, projection, model, color=False):
+    vertices = obj.vertices
+    scale_matrix = np.eye(3) * 3
+    h, w = model.shape
+    for face in obj.faces:
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+        points = np.dot(points, scale_matrix)
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+        imgpts = np.int32(dst)
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, DEFAULT_COLOR)
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1]
+            cv2.fillConvexPoly(img, imgpts, color)
+    return img
+
+
+def projection_matrix(camera_params, homography):
+    homography *= -1
+    rotate_and_translate = np.dot(np.linalg.inv(camera_params), homography)
+    col_1 = rotate_and_translate[:, 0]
+    col_2 = rotate_and_translate[:, 1]
+    col_3 = rotate_and_translate[:, 2]
+    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+    rotate_1 = col_1 / l
+    rotate_2 = col_2 / l
+    translation = col_3 / l
+    c = rotate_1 + rotate_2
+    p = np.cross(rotate_1, rotate_2)
+    d = np.cross(c, p)
+    rotate_1 = np.dot(
+        c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2)
+    )
+    rotate_2 = np.dot(
+        c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2)
+    )
+    rotate_3 = np.cross(rotate_1, rotate_2)
+    projection = np.stack((rotate_1, rotate_2, rotate_3, translation)).T
+    return np.dot(camera_params, projection)
+
+
+def hex_to_rgb(hex):
+    hex_color = hex.lstrip("#")
+    h_len = len(hex_color)
+    return tuple(
+        int(hex_color[i : i + h_len // 3], 16) for i in range(0, h_len, h_len // 3)
+    )
 
 
 parser = argparse.ArgumentParser(description="app")
