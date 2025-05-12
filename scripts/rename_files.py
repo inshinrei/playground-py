53271e9d3064986274946a2f5dba9ff4a3ee4448
tokkiumai <kiwidancebad@gmail.com>
2025-05-12 11:43:11 +0300

    rename test

--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -1,9 +1,28 @@
 import os
+import uuid
 
 run_dir = os.path.abspath(os.getcwd())
+allowed_extensions = (".png", ".heic", ".jpg", ".jpeg")
 
-for file in os.listdir(run_dir):
-    filename, fileext = os.path.splitext(file)
-    print(filename)
-    print(fileext)
-    print(os.path.isdir(file))
+
+def run_rename(dir=run_dir):
+    print(dir)
+    for file in os.listdir(dir):
+        path = dir + "/" + file
+        if os.path.isdir(path):
+            run_rename(path)
+            continue
+
+        file_name, file_extension = os.path.splitext(file)
+        if file_extension in allowed_extensions:
+            new_name = dir + "/" + str(uuid.uuid4().hex) + file_extension
+            rename_file(path, new_name)
+        else:
+            print("skipped, unsupported file ext: " + file)
+
+
+def rename_file(file, new_name):
+    os.rename(file, new_name)
+
+
+run_rename()
