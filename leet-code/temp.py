b14c96a588faba1baa4e711364e4f0f4a167c989
tokkiumai <kiwidancebad@gmail.com>
2026-05-20 08:07:34 +0300

    152

--- leet-code/temp.py
+++ leet-code/temp.py
@@ -1,12 +1,12 @@
 from typing import List
 
 
-def max_sub(nums: List[int]) -> List[int]:
-    max_sub = nums[0]
-    curr_sum = 0
+def test(nums: List[int]) -> List[int]:
+    res = max(nums)
+    curr_min, curr_max = 1, 1
     for n in nums:
-        if curr_sum < 0:
-            curr_sum = 0
-        curr_sum += n
-        max_sub = max(max_sub, curr_sum)
-    return max_sub
+        tmp_max = n * curr_max
+        curr_max = max(n * curr_max, n * curr_min, n)
+        curr_min = max(tmp_max, n * curr_min, n)
+        res = max(res, curr_max, curr_min)
+    return res
