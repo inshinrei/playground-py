7380576d4d159ad3bdfbde76a3413b80fcee2481
tokkiumai <kiwidancebad@gmail.com>
2026-05-21 22:47:36 +0300

    33

--- leet-code/temp.py
+++ leet-code/temp.py
@@ -2,16 +2,23 @@ from typing import List
 
 
 def test(nums: List[int]) -> List[int]:
-    res = nums[0]
     l, r = 0, len(nums) - 1
     while l <= r:
-        if nums[l] < nums[r]:
-            res = min(res, nums[l])
-            break
-        m = (l + r) // 2
-        res = min(res, nums[m])
-        if nums[m] >= nums[l]:
-            l = m + 1
+        mid = (l + r) // 2
+        if target == nums[mid]:
+            return mid
+        if nums[mid] >= nums[l]:
+            if target > nums[mid]:
+                l = mid + 1
+            elif target < nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
         else:
-            r = m - 1
-    return res
+            if target < nums[mid]:
+                r = mid - 1
+            elif target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+    return -1
